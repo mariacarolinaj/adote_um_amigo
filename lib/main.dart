805613f834eea21b3900ecc58a1a31976db5371c
Maria Carolina <mariacarolina_bh@hotmail.com>
2022-09-11 17:31:54 -0300

    Define primeiras configurações de style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:adote_um_amigo/app/shared/rotas.dart';
+import 'package:adote_um_amigo/shared/rotas.dart';
+import 'package:adote_um_amigo/shared/style.dart';
 import 'package:flutter/material.dart';
 import 'app/cadastros/cadastro-animal_page.dart';
 
@@ -15,7 +16,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light, // light mode
+        primaryColor: Cores.primaria,
+        fontFamily: 'Jost',
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+              color: Cores.titulo, fontSize: 32, fontStyle: FontStyle.normal),
+          titleMedium: TextStyle(
+              color: Cores.titulo, fontSize: 24, fontStyle: FontStyle.normal),
+          titleSmall: TextStyle(
+              color: Cores.titulo, fontSize: 16, fontStyle: FontStyle.normal),
+          bodyMedium: TextStyle(
+              color: Cores.texto, fontSize: 14, fontStyle: FontStyle.normal),
+          bodySmall: TextStyle(
+              color: Cores.texto, fontSize: 12, fontStyle: FontStyle.normal),
+        ),
       ),
       home: const MyHomePage(title: 'Adote um Amigo'),
       // definir as rotas para cada página aqui
---
+++ lib/shared/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Cores {
+  static const primaria = Color.fromARGB(255, 255, 154, 0);
+  static const secondary = Color.fromARGB(255, 89, 11, 85);
+  static const background = Colors.white;
+  static const titulo = Color.fromARGB(255, 82, 102, 90);
+  static const texto = Color.fromARGB(255, 82, 102, 90);
+  static const textoBotao = Colors.white;
+}
