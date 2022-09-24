cb30f3927ef2920f1ba35dc701c0b637ec5e0c1e
Maria Carolina <mariacarolina_bh@hotmail.com>
2022-09-23 22:54:56 -0300

    Termina implementação do perfil do animal

--- lib/app/perfil-animal/perfil-animal_page.dart
+++ lib/app/perfil-animal/perfil-animal_page.dart
@@ -34,7 +34,19 @@ class PerfilAnimalPageState extends State<PerfilAnimalPage> {
             SliverFillRemaining(
               hasScrollBody: false,
               child: Column(
-                children: <Widget>[_buildCarouselFotos(), _buildSecaoInicial()],
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        _buildCarouselFotos(),
+                        _buildSecaoInicial(),
+                        _buildCardDistancia(),
+                        _buildInformacoes(),
+                      ],
+                    ),
+                  ),
+                  _buildButton(),
+                ],
               ),
             ),
           ],
@@ -117,39 +129,157 @@ class PerfilAnimalPageState extends State<PerfilAnimalPage> {
   Widget _buildSecaoInicial() {
     return Column(
       children: <Widget>[
-        _buildNome(),
+        _buildTituloSecao(widget.animal.nome),
         _buildCaracteristicas(),
       ],
     );
   }
 
-  Widget _buildNome() {
-    return RichText(
-      text: TextSpan(
-          text: widget.animal.nome,
+  Widget _buildTituloSecao(String tituloSecao) {
+    return Container(
+      margin: const EdgeInsets.only(top: 16, bottom: 16),
+      child: Text(tituloSecao,
           style: const TextStyle(
               color: Cores.titulo,
               fontSize: 24,
               fontFamily: 'Jost',
-              fontStyle: FontStyle.normal),
-          children: const <TextSpan>[]),
+              fontStyle: FontStyle.normal)),
     );
   }
 
   Widget _buildCaracteristicas() {
-    return RichText(
-      text: TextSpan(
-          text: widget.animal.caracteristicas,
-          style: const TextStyle(
-              color: Cores.titulo,
-              fontSize: 16,
-              fontFamily: 'Jost',
-              fontStyle: FontStyle.normal),
-          children: const <TextSpan>[]),
+    return Text(widget.animal.caracteristicas,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+            color: Cores.titulo,
+            fontSize: 16,
+            fontFamily: 'Jost',
+            fontStyle: FontStyle.normal));
+  }
+
+  Widget _buildCardDistancia() {
+    return Container(
+      margin: const EdgeInsets.only(top: 32, bottom: 16),
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 80,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color.fromARGB(255, 211, 212, 255),
+      ),
+      child: Center(
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: const TextStyle(color: Cores.texto, fontSize: 18),
+            children: [
+              const WidgetSpan(
+                child: Icon(
+                  Icons.pin_drop_outlined,
+                  size: 18,
+                  color: Cores.texto,
+                ),
+              ),
+              TextSpan(
+                text: "  A ${_getDistancia()} de você",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  // Widget _buildInformacoes() {
-  //   return
-  // }
+  Widget _buildInformacoes() {
+    return Column(
+      children: [
+        _buildTituloSecao("Informações"),
+        Center(
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3 * 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    _getTextInfoDescricao("Idade"),
+                    _getTextInfoDescricao("Cidade"),
+                    _getTextInfoDescricao("Vacinas")
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3 * 1.3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _getTextInfoValor("${widget.animal.idade} anos"),
+                    _getTextInfoValor('bh'),
+                    _getTextInfoValor(widget.animal.vacinas)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getTextInfoDescricao(String descricao) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Text(
+        descricao,
+        style: const TextStyle(
+            color: Cores.titulo,
+            fontSize: 16,
+            fontFamily: 'Jost',
+            fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.normal),
+      ),
+    );
+  }
+
+  _getTextInfoValor(String valor) {
+    return Text(
+      valor,
+      style: const TextStyle(
+          color: Cores.titulo,
+          fontSize: 16,
+          fontFamily: 'Jost',
+          fontStyle: FontStyle.normal),
+    );
+  }
+
+  Widget _buildButton() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0),
+        child: SizedBox(
+          width: 300,
+          height: 50,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Cores.primaria,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+            child: const Text(
+              'Tenho interesse',
+              style: TextStyle(fontSize: 16),
+            ),
+            onPressed: () {
+              // redirecionar pro chat com o tutor do animal
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _getDistancia() {
+    // implementar chamada à api para calculo da distancia
+    return "5km";
+  }
 }
