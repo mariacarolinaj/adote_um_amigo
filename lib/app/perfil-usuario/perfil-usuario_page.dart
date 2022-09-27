7467626be0e1bddf5e07bebe5addc007c1ac11a8
Viktor <viktorguilherme@gmail.com>
2022-09-27 11:25:46 -0300

    Fix leading and trailing margin

--- lib/app/perfil-usuario/perfil-usuario_page.dart
+++ lib/app/perfil-usuario/perfil-usuario_page.dart
@@ -60,106 +60,112 @@ class PerfilUsuarioPageState extends State<PerfilUsuarioPage> {
 
   //imagem do fundo/ capa
   Widget buildCoverImage() => Container(
-    color: Colors.grey,
-    child: Image.network(
-      'https://i.pinimg.com/originals/c2/da/7a/c2da7af65cbcfaff6f7582d6a0b79781.jpg',
-      width: double.infinity,
-      height: coverHeight,
-      fit: BoxFit.cover,
-    ),
-  ); //Container
+        color: Colors.grey,
+        child: Image.network(
+          'https://i.pinimg.com/originals/c2/da/7a/c2da7af65cbcfaff6f7582d6a0b79781.jpg',
+          width: double.infinity,
+          height: coverHeight,
+          fit: BoxFit.cover,
+        ),
+      ); //Container
 
   //imagem de perfil/ avatar
   Widget buildProfileImage() => CircleAvatar(
-    radius: profileHeight / 2,
-    backgroundColor: Colors.grey.shade800,
-    backgroundImage: NetworkImage(
-      'https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_960_720.jpg',
-    ),
-  );
+        radius: profileHeight / 2,
+        backgroundColor: Colors.grey.shade800,
+        backgroundImage: NetworkImage(
+          'https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_960_720.jpg',
+        ),
+      );
 
   Widget buildContent() => Column(
-    children: [
-      const SizedBox(height: 8),
-      Text(
-        'Ana',
-        style: TextStyle(fontSize: 28, color: Colors.black, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(height: 8),
-      Text(
-        'Somos uma instituição de caridade, onde nosso proposito é buscar animais que foram abonados e encontar um novo lar para os animaiszinhos!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
-      ),
-
-      const SizedBox(height: 16),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          buildSocialIcon(FontAwesomeIcons.dog, Rotas.meusAnimais),
-          SizedBox(width: 12),
-          buildSocialIcon(FontAwesomeIcons.searchLocation, Rotas.meusAnimais),  //adicionar rota de localizacao
-          SizedBox(width: 12),
-          buildSocialIcon(FontAwesomeIcons.star, Rotas.meusAnimais), //adicionar rota de quantas estrelas
+          const SizedBox(height: 8),
+          Text(
+            'Ana',
+            style: TextStyle(
+                fontSize: 28, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            child: Text(
+              'Somos uma instituição de caridade, onde nosso proposito é buscar animais que foram abonados e encontar um novo lar para os animaiszinhos!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            margin: EdgeInsets.only(left: 32, right: 32),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildSocialIcon(FontAwesomeIcons.dog, Rotas.meusAnimais),
+              SizedBox(width: 12),
+              buildSocialIcon(FontAwesomeIcons.searchLocation,
+                  Rotas.meusAnimais), //adicionar rota de localizacao
+              SizedBox(width: 12),
+              buildSocialIcon(FontAwesomeIcons.star,
+                  Rotas.meusAnimais), //adicionar rota de quantas estrelas
+            ],
+          ),
+          const SizedBox(height: 16),
+          Divider(),
+          const SizedBox(height: 16),
+          NumbersWidget(),
+          const SizedBox(height: 16),
+          Divider(),
+          const SizedBox(height: 16),
+          buildAbout(),
+          const SizedBox(height: 32),
+          buildMyAnimal(),
+          const SizedBox(height: 32),
         ],
-      ),
-
-      const SizedBox(height: 16),
-      Divider(),
-      const SizedBox(height: 16),
-      NumbersWidget(),
-
-      const SizedBox(height: 16),
-      Divider(),
-      const SizedBox(height: 16),
-      buildAbout(),
-      const SizedBox(height: 32),
-      buildMyAnimal(),
-      const SizedBox(height: 32),
-    ],
-  );
+      );
 
   //icones de informacao do usuario
   Widget buildSocialIcon(IconData icon, String rotas) => CircleAvatar(
-    radius: 25,
-    child: Material(
-      shape: CircleBorder(),
-      clipBehavior: Clip.hardEdge,
-      color: Colors.transparent,
-      child: InkWell(
-        onTap: () => Navigator.pushNamed(context, rotas),
-        child: Center(child: Icon(icon, size: 32)),
-      ),
-    ),
-  );
+        radius: 25,
+        child: Material(
+          shape: CircleBorder(),
+          clipBehavior: Clip.hardEdge,
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () => Navigator.pushNamed(context, rotas),
+            child: Center(child: Icon(icon, size: 32)),
+          ),
+        ),
+      );
 
   //botao para entrar em contato com um usuario
   Widget buildAbout() => Container(
-    child: TextButton(
-      style: TextButton.styleFrom(
-          backgroundColor: Colors.orangeAccent,
-          elevation: 30,
-          shadowColor: Colors.green),
-      child: Text(
-        'Entrar em contato',
-        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-      ),
-      onPressed: () {},  //colocar rota do chat
-    ),
-  );
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              elevation: 30,
+              shadowColor: Colors.green),
+          child: Text(
+            'Entrar em contato',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          onPressed: () {}, //colocar rota do chat
+        ),
+      );
 
   //botao para meus animais de interesse *Futuramente colocar em um if para visao do usuario somente
   Widget buildMyAnimal() => Container(
-    child: TextButton(
-      style: TextButton.styleFrom(
-          backgroundColor: Colors.orangeAccent,
-          elevation: 30,
-          shadowColor: Colors.green),
-      child: Text(
-        'Meus animais de interesse',
-        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-      ),
-      onPressed: () => Navigator.pushNamed(context, Rotas.meusAnimais),
-    ),
-  );
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              elevation: 30,
+              shadowColor: Colors.green),
+          child: Text(
+            'Meus animais de interesse',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          onPressed: () => Navigator.pushNamed(context, Rotas.meusAnimais),
+        ),
+      );
 }
