0b321f1d907f111e0de296f00d1c7ab653997004
luana <luanaplins@gmail.com>
2022-11-01 09:18:27 -0300

    Added Models to Model Folder
    Created the model according to projected data structure.

--- lib/models/animal.dart
+++ lib/models/animal.dart
@@ -4,10 +4,11 @@ class Animal {
   late String caracteristicas;
   late String vacinas;
   late int idade;
+  late int idAnimal;
   late var fotos = <String>[]; // base64
 
   Animal.empty();
 
-  Animal(this.nome, this.raca, this.caracteristicas, this.vacinas, this.idade,
-      this.fotos);
+  Animal(this.idAnimal, this.nome, this.raca, this.caracteristicas,
+      this.vacinas, this.idade, this.fotos);
 }
---
+++ lib/models/person.dart
@@ -0,0 +1,13 @@
+class Person {
+  late String nome;
+  late String email;
+  late String password;
+  late double latGeo;
+  late double lonGeo;
+  late int idPerson;
+
+  Person.empty();
+
+  Person(this.nome, this.email, this.password, this.latGeo, this.lonGeo,
+      this.idPerson);
+}
