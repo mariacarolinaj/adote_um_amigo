91fa9f09e435fcf90744d6c8040b71fcec18108e
Maria Carolina <mariacarolina_bh@hotmail.com>
2022-11-06 16:34:14 -0300

    Correção das funções de DB para Animal

--- lib/models/animal.dart
+++ lib/models/animal.dart
@@ -6,11 +6,22 @@ class Animal {
   late String vacinas;
   late int idade;
   late var fotos = <String>[]; // base64
-  late String donoId;
+  late int donoId;
   late String tipo;
 
   Animal.empty();
 
   Animal(this.id, this.nome, this.raca, this.caracteristicas, this.vacinas,
-      this.idade, this.fotos, this.tipo);
+      this.idade, this.fotos, this.donoId, this.tipo);
+
+  Animal.fromMap(Map<String, dynamic> map) {
+    id = map["id"];
+    nome = map["nome"];
+    raca = map["raca"];
+    caracteristicas = map["caracteristicas"];
+    vacinas = map["vacinas"];
+    idade = map["idade"];
+    donoId = map["donoId"];
+    tipo = map["tipo"];
+  }
 }
