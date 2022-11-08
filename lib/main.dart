202f4f5e5f79f640173da85362e814d93703b21e
Eduarda Oliveira <oeduarda239@gmail.com>
2022-11-08 11:19:55 -0300

    remove navbar

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ? const ListagemAnimaisPage()
             : const loginUsuarioPage();
       },
-      bottomNavigationBar: BarraNavegacaoInferior(),
     );
   }
 
